9307c9734591288d2bfd1decc83c8e0a42610044
theNerd247 <noah.harvey247 gmail>
2015-12-27 14:38:19 -0500

    started added functionality to the Budget module

--- src/Data/Budget/Budget.hs
+++ src/Data/Budget/Budget.hs
@@ -23,6 +23,7 @@ where
 
 import YabCommon
 import qualified Data.Map as DM
+import Data.Budget.Entry
 
 type BudgetAccount = DM.Map Name Amount
 
@@ -36,3 +37,26 @@ data Budget = Budget
     ,budgetRate    :: Rate
   }
   deriving (Generic,Typeable,Show,Read,Eq,Ord)
+
+
+
+checkBudget :: (MonadIO m) => Budget -> m ()
+checkBudget (Budget {budgetAccounts = as, income = i}) = 
+  let bal = i - (sum as)
+  in case bal >= 0 of
+    True -> putStrLn "Budget is balanced!" 
+    False -> putStrLn "Budget is unbalanced: " ++ (show bal)
+
+
+-- | Checks the given account to make sure that it's balance isn't negative
+checkAccount :: (MonadIO m) => Name -> Amount -> Entries -> m ()
+checkAccount accName i es = 
+  let bal = Prelude.foldr ((+) . entryAmount) 0 es
+  in case bal >= 0 of
+    True -> putStrLn $ "Account: " ++ accName ++ " is ok!"
+    False -> putStrLn $ "Account: " ++ accName ++ "is off! Min payoff: " 
+      ++ (show $ findMinPayOff bal i)
+
+findMinPayOff total rate
+  | total >= 0 || rate == 0 = 0
+  | otherwise = abs(total)/rate
