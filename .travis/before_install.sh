696a51cd563be942714ee68dfa63e5c9b55b2a47
theNerd247 <noah.harvey247 gmail>
2016-06-02 21:12:12 -0400

    removed yaml literal syntax in scripts
      * this should build now

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -12,24 +12,24 @@ if [ "x$GHCVER" = "xhead" ]; then CABALARGS=--allow-newer; fi
 
 # Download and unpack the stack executable
 export PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:$HOME/.local/bin:/opt/alex/$ALEXVER/bin:/opt/happy/$HAPPYVER/bin:$HOME/.cabal/bin:$PATH
-mkdir -p ~/.local/bin
-|
-  if [ `uname` = "Darwin" ]
-  then
-    travis_retry curl --insecure -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin
-  else
-    travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
-  fi
+mkdir -p ~/.local/bin 
 
-  # Use the more reliable S3 mirror of Hackage
-  mkdir -p $HOME/.cabal
-  echo 'remote-repo: hackage.haskell.org:http://hackage.fpcomplete.com/' > $HOME/.cabal/config
-  echo 'remote-repo-cache: $HOME/.cabal/packages' >> $HOME/.cabal/config
+if [ `uname` = "Darwin" ]
+then
+	travis_retry curl --insecure -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin
+else
+	travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+fi
 
-  if [ "$CABALVER" != "1.16" ]
-  then
-    echo 'jobs: $ncpus' >> $HOME/.cabal/config
-  fi
+# Use the more reliable S3 mirror of Hackage
+mkdir -p $HOME/.cabal
+echo 'remote-repo: hackage.haskell.org:http://hackage.fpcomplete.com/' > $HOME/.cabal/config
+echo 'remote-repo-cache: $HOME/.cabal/packages' >> $HOME/.cabal/config
+
+if [ "$CABALVER" != "1.16" ]
+then
+	echo 'jobs: $ncpus' >> $HOME/.cabal/config
+fi
 
 # Get the list of packages from the stack.yaml file
 PACKAGES=$(stack --install-ghc query locals | grep '^ *path' | sed 's@^ *path:@@')
