36578a8737d2e7b77d602f111418209164d68373
theNerd247 <noah.harvey247 gmail>
2016-06-02 20:59:24 -0400

    added travis scripts
      * makes managing the .travis.yml a bit cleaner
      * removed uneeded cabal and OSX builds ...we're not ready for them.

---
+++ .travis/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#copied from .travis.yml on the stack website:
+# http://docs.haskellstack.org/en/stable/GUIDE/#travis-with-caching
+
+echo "$(ghc --version) [$(ghc --print-project-git-commit-id 2> /dev/null || echo '?')]"
+if [ -f configure.ac ]; then autoreconf -i; fi
+|
+  set -ex
+  case "$BUILD" in
+    stack)
+      stack --no-terminal --install-ghc $ARGS test --bench --only-dependencies
+      ;;
+    cabal)
+      cabal --version
+      travis_retry cabal update
+      cabal install --only-dependencies --enable-tests --enable-benchmarks --force-reinstalls --ghc-options=-O0 --reorder-goals --max-backjumps=-1 $CABALARGS $PACKAGES
+      ;;
+  esac
+  set +ex
+
+
