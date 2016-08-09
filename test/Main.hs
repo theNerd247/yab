6c31b982f761c5a529936be63f2526b3d294ce52
theNerd247 <noah.harvey247 gmail>
2016-08-09 17:01:06 -0400

    removed uneeded files and removed old unit tests

--- test/Main.hs
+++ test/Main.hs
@@ -7,24 +7,18 @@ import Test.Tasty.Ingredients.Rerun
 import Test.Tasty.QuickCheck
 
 import Test.Data.Budget
-import Test.Data.Serialization
-import Test.Data.Serialization.Csv
-import Test.Yab.AccountSort
 
-import Data.Budget
-import Data.Serialization
+main = undefined
 
-import YabCommon
+{-main = defaultMainWithIngredients -}
+  {-[-}
+    {-rerunningTests defaultIngredients-}
+  {-] tests-}
 
-main = defaultMainWithIngredients 
-  [
-    rerunningTests defaultIngredients
-  ] tests
-
-tests :: TestTree 
-tests = testGroup "Tests" [testGroup n ts | (n,ts) <- [
-  ("Serialize",serializeTests)
-  ,("Budget",budgetTests)
-  ,("CSV",csvTests)
-  ,("AccountSort",accountSortTests)
-  ]]
+{-tests :: TestTree -}
+{-tests = testGroup "Tests" [testGroup n ts | (n,ts) <- [-}
+  {-("Serialize",serializeTests)-}
+  {-,("Budget",budgetTests)-}
+  {-,("CSV",csvTests)-}
+  {-,("AccountSort",accountSortTests)-}
+  {-]]-}
